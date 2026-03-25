843465b2cdea0f7dba9ca31636ca5fe98d0c2fdb
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-24 19:58:50 -0700

    Update to 26.1

---
+++ src/main/java/archives/tater/eatbottle/mixin/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package archives.tater.eatbottle.mixin;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/archives/tater/eatbottle/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package archives.tater.eatbottle;
+
+import org.jspecify.annotations.NullMarked;
